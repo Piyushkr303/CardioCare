a8a69e15dc1bb5a3cc6ac0de1fe4a4f9535e0913
Piyush Kumar <90102996+Piyushkr303@users.noreply.github.com>
2025-04-08 10:33:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from main import ann_app
 from heart-disease-rbm-classifier import rbm_app
-from from heart-disease-voting-classifier import voting_clf_app
+from heart-disease-voting-classifier import voting_clf_app
 
 
 def main():
